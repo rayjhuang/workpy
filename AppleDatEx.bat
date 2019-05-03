1ae69d6faae72311aadf4389ef6812c384f637c0
ray <rayhuang@canyon-semi.com.tw>
2019-05-03 18:41:06 +0800

    add

---
+++ AppleDatEx.bat
@@ -0,0 +1,24 @@
+
+python -m cynpy.csp 1 prog_raw 06 00 00 20 00 02 08 00 30 00 18 01 50 00 10 02
+python -m cynpy.csp 1 prog_raw 60 00 10 03 70 00 18 04 90 00 10 05 a0 00 10 06
+python -m cynpy.csp 1 prog_raw b0 00 10 07 c0 00 10 08 ff ff ff ff ff ff ff ff
+python -m cynpy.csp 1 prog_raw 43 34 48 36 34 33 35 30 41 31 47 48 36 37 34 41
+python -m cynpy.csp 1 prog_raw 4c 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw 41 70 70 6c 65 20 49 6e 63 2e 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+python -m cynpy.csp 1 prog_raw 36 31 57 20 55 53 42 2d 43 20 50 6f 77 65 72 20
+python -m cynpy.csp 1 prog_raw 41 64 61 70 74 65 72 00 00 00 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw 30 78 31 36 35 00 00 00 00 00 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+python -m cynpy.csp 1 prog_raw 30 31 30 39 30 30 31 31 00 00 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw 31 2e 30 00 00 00 00 00 00 00 00 00 00 00 00 00
+python -m cynpy.csp 1 prog_raw 02 00 00 00 00 02 00 00 ff ff ff ff ff ff ff ff
+python -m cynpy.csp 1 prog_raw 03 00 00 00 00 02 12 00 ff ff ff ff ff ff ff ff
+
+python -m cynpy.csp 1 reset
+
+
+
+
+
+
