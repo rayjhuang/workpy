ada3d32ef113e73b55f7208458f7d4e1855e755d
ray <rayhuang@canyon-semi.com.tw>
2019-09-27 19:11:06 +0800

    add comments in install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,14 @@
+REM ============================================
+REM 1. work in .\ directory
+REM    > install cynpy
+REM    > python -mcynpy.csp
+REM
+REM 2. work in .\cynpy directory
+REM    > python -B .\cynpy\csp.py
+REM
+REM ============================================
 
 cd .\%1
-rmdir /s .\build
+rmdir /s/q .\build
 python setup.py install --skip-build --no-compile
 cd ..\
