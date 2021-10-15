86b7411af039361a8a7a4b0e3e8ea0195e7f6e35
rayjhuang <rayjhuang@msn.com>
2021-10-15 17:05:39 +0800

    CYF24A0_Tmk0a170.bin programmer

--- init_win1.bat
+++ init_win1.bat
@@ -13,9 +13,12 @@ rem SET MYPY=%MYPYPATH%\rapy
 
 rem SET PYTHONPATH=%TOTALPHASEPATH%\aardvark-api-windows-i686-v5.13\python
 
+rem If like to X:\project\github\workpy\cynpy>python -B .\cynpy\aardv.py
+rem Include current directory for module searching
     SET PYTHONPATH=%CD%;%TOTALPHASEPATH%\aardvark-api-windows-x86_64-v5.13\python
+
+rem for DLL, EXE searching
+    @PATH=%CD%;%PYPATH%;%TOTALPHASEPATH%\aardvark-api-windows-i686-v5.13\python;%PATH%
 
     dir/w %PYTHONPATH%
 rem dir/w %MYPY%
-
-    @PATH=%CD%;%PYPATH%;%PATH%
