751db2a8f510beec9e2bbb9288cd9a32af316197
rayjhuang <rayjhuang@msn.com>
2023-10-31 17:18:10 +0800

    try the new token BTW

--- writer_cyf24a0.bat
+++
@@ -1,7 +0,0 @@
-
-
-REM python -B cynpy\csp.py stop
-REM python -B cynpy\csp.py trim
-    python -B cynpy\csp.py nvmsegm 0   hiv=2 intr=7 ..\CYF24A0_Tmk0a170.bin
-    python -B cynpy\csp.py nvmprog 930 hiv=2 \PY1124%DATE:~2,2%%DATE:~5,2%%DATE:~8,2%%TIME:~0,2%%TIME:~3,2%
-
